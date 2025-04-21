1746b81ecf330964d2bb238a3ed9f55852bfd98a
𝑴𝒊𝒔𝒕𝒆𝒓 𝑯𝒐𝒍𝒀 <misterholyexample111@gmail.com>
2025-04-21 17:59:51 +0300

    Update IrisFarm.py

--- IrisFarm.py
+++ IrisFarm.py
@@ -26,8 +26,8 @@ class IrisfarmMod(loader.Module):
         self.client = client
         self.farm_status = self.db.get("Irisfarm", "status", {})  
 
-        if self.farm_status.get("chat"):
-            asyncio.create_task(self._farm_loop("chat"))
+        if self.farm_status.get("chat") and self.farm_status.get("chat_id"):
+            asyncio.create_task(self._farm_loop("chat", self.farm_status["chat_id"]))
         if self.farm_status.get("bot"):
             asyncio.create_task(self._farm_loop("bot"))
 
@@ -35,13 +35,15 @@ class IrisfarmMod(loader.Module):
         """- вкл/выкл фарму в текущем чате"""
         if self.farm_status.get("chat"):
             self.farm_status["chat"] = False
+            self.farm_status["chat_id"] = None
             self.db.set("Irisfarm", "status", self.farm_status)
             await utils.answer(message, "<emoji document_id=5420323339723881652>⚠️</emoji> <b>Фарма в чате остановлена.</b>")
         else:
             self.farm_status["chat"] = True
+            self.farm_status["chat_id"] = message.chat.id
             self.db.set("Irisfarm", "status", self.farm_status)
             await utils.answer(message, "<b>Фарма ☢️IC в чате запущена.</b>")
-            asyncio.create_task(self._farm_loop("chat"))
+            asyncio.create_task(self._farm_loop("chat", message.chat.id))
 
     async def farmiriscmd(self, message):
         """- вкл/выкл фарму в лс бота"""
@@ -55,12 +57,12 @@ class IrisfarmMod(loader.Module):
             await utils.answer(message, "<b>Фарма ☢️IC в лс бота запущена.</b>")
             asyncio.create_task(self._farm_loop("bot"))
 
-    async def _farm_loop(self, mode):
+    async def _farm_loop(self, mode, chat_id=None):
         while self.farm_status.get(mode):
             try:
-                if mode == "chat":
-                    await self.client.send_message("me", "Фарма")
-                else:
+                if mode == "chat" and chat_id:
+                    await self.client.send_message(chat_id, "Фарма")
+                elif mode == "bot":
                     await self.client.send_message(_bot, "Фарма")
                 await asyncio.sleep(14700)  
             except Exception:
